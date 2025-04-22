0c4e90927144e6900481dd7491fa322a8050f8ca
gab-jrz <gabriela.gajerez@gmail.com>
2025-04-22 15:20:17 -0300

    Add files via upload
    Primer commit - Base SW

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SwapWeb___PF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
